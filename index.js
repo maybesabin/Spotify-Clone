0e7f17b8c24da82684c685e5146acfda3bb91ba9
Sabin Hamal <highsabin987@gmail.com>
2024-01-27 19:30:40 +0545

    Basic Layout Done

---
+++ index.js
@@ -0,0 +1,10 @@
+var mainContent = document.getElementById("mainContent");
+var item = document.getElementById("item");
+
+mainContent.style.background = "linear-gradient(#1a1d1a, black)"
+mainContent.style.transition = ".4s"
+
+function bgred(){
+    mainContent.style.background = "linear-gradient(red,black)"
+    
+}
